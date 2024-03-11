ca86bd4a0cc28c2ec51e4031a62aa8d3d1486f14
LeeJunYoung0315 <162395983+LeeJunYoung0315@users.noreply.github.com>
2024-03-11 14:33:58 +0900

    create mainpage

---
+++ front/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import ShopCategory from './among/ShopCategory';
+import SubFoot from './foot/SubFoot';
+import WhatEtsy from './foot/WhatEtsy';
+import Header from './headers/Header';
+import Et from './foot/Et';
+import AppDown from './foot/AppDown';
+import LastF from './foot/LastF';
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ShopCategory />
+      <WhatEtsy />
+      <SubFoot />
+      <Et />
+      <AppDown />
+      <LastF />
+    </div>
+  );
+}
+
+export default App;
